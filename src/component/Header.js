0edd78548423a3b8eb962474de85d297ec564296
markyul <pig052656@naver.com>
2021-12-07 20:50:58 +0900

    react 초기화 후 Header 구현

---
+++ src/component/Header.js
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <div>
+      <header className="header-nav">
+        <div className="container side-flex">
+          <section className="home">RYUL - Portfolio</section>
+          <section className="side-flex">
+            <div className="nav-item">About me</div>
+            <div className="nav-item">Skill</div>
+            <div className="nav-item">Project</div>
+            <div className="nav-item">Career</div>
+          </section>
+        </div>
+      </header>
+    </div>
+  );
+}
