67ef948b57a5fdcd5b71cad853ac4f1c1e5f0b19
markyul <pig052656@naver.com>
2021-12-10 20:40:19 +0900

    About Me 구현

---
+++ src/components/AboutMe.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <div className="about-background">
+      <div className="container content">
+        <section className="name">궁금한 것이 많은 개발자</section>
+        <section className="name">"김 학 률"</section>
+        <section className="introduce">
+          소통을 가장 중요시하며 분위기 자체가 밝은 사람입니다.
+        </section>
+        <hr className="about-line"></hr>
+        <section className="reference">
+          <article>
+            <header>Github: </header>
+            <span
+              onClick={() =>
+                window.open("https://github.com/markyul", "_blank")
+              }
+            >
+              https://github.com/markyul
+            </span>
+          </article>
+          <article>
+            <header>Email: </header>
+            <span>pig68111000@gmail.com</span>
+          </article>
+        </section>
+      </div>
+    </div>
+  );
+}
