a4d8d7da9f0f08b1d7436219d8ba9552776ef3d5
markyul <pig68111000@gmail.com>
2021-12-18 22:15:33 +0900

    design 수정
    카드 그림자, nav-item 글자 크기 수정

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Card(props) {
   return (
     <article className="card">
-      <img src={props.src} height="140" width="364" alt="logo" />
+      <img src={props.src} height="100" width="300" alt="logo" />
     </article>
   );
 }
