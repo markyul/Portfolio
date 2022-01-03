f20b63e88175176c0e8b601f43afb7dc9e3c2a98
markyul <pig052656@naver.com>
2022-01-03 23:09:40 +0900

    project 객체화
    map으로 component 반복

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,12 +4,28 @@ import corner_archive1 from "../asset/corner_archive1.jpg";
 import catlas from "../asset/catlas1.png";
 
 export default function Projects() {
+  const projects = [
+    {
+      name: "Corner Archive",
+      img: corner_archive1,
+    },
+    {
+      name: "Catlas",
+      img: catlas,
+    },
+  ];
+
   return (
     <div className="projects-background">
       <div className="container">
         <header className="container-title">Project</header>
-        <ProjectCard name="Corner Archive" img={corner_archive1} />
-        <ProjectCard name="Catlas" img={catlas} />
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.name}
+            name={project.name}
+            img={project.img}
+          />
+        ))}
       </div>
     </div>
   );
